961d88dd8409c5d943c12bd4520bbe2b107c9ed8
starstuff <evenstarstuff@gmail.com>
2026-03-26 16:24:56 -0700

    Create birb-class

---
+++ scripts/build.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+
+import yaml
+from ryland import Ryland
+
+ROOT_DIR = Path(__file__).parent.parent
+OUTPUT_DIR = ROOT_DIR / "site"
+TEMPLATE_DIR = ROOT_DIR / "templates"
+PANTRY_DIR = ROOT_DIR / "pantry"
+DATA_DIR = ROOT_DIR / "data"
+
+ryland = Ryland(output_dir=OUTPUT_DIR, template_dir=TEMPLATE_DIR)
+
+ryland.clear_output()
+
+##style
+
+ryland.copy_to_output(PANTRY_DIR / "style.css")
+ryland.add_hash("style.css")
+
+## birds
+
+for yaml_file in DATA_DIR.glob("*.yaml"):
+    bird = yaml.safe_load(open(yaml_file))
+    ryland.render_template("bird.html", f"birds/{bird['name'].lower().replace(' ', '-')}/index.html", {
+        "bird": bird,
+    })
+
+## home page
+
+ryland.render_template("home.html", "index.html", {
+    "birds": [yaml.safe_load(open(f)) for f in DATA_DIR.glob("*.yaml")],
+})
